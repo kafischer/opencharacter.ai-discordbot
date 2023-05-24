d5e534c3913c13114ceb5cccb769d65895d6e060
kevinfischer <fischer.kevin.a@gmail.com>
2023-05-24 19:24:33 -0400

    Bot v0

--- bot.js
+++ bot.js
@@ -5,18 +5,15 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async message => {
     if (message.author.bot) return;
 
     console.log('got message', message);
     if (message.channel.id === '1111061024645320834') {
-        const embed = new MessageEmbed()
-            .setColor('#0099ff') // Set the color
-            .setTitle('New message received') // Set the title
-            .setDescription(`Message content: ${message.content}`) // Set the description
-            .setAuthor('Samantha 2.0', "https://lexica.art/prompt/369f5877-5c72-4c3a-9da6-19a8766d2657");
-
-       message.channel.send({ embeds: [embed] });
+        await client.user.setUsername('New_Username');
+        const avatar = 'https://cdn-icons-png.flaticon.com/512/4711/4711987.png';
+        await client.user.setAvatar(avatar);
+       message.channel.send("Hello!");
     }
 });
 
