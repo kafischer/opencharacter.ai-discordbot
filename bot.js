ac91e4d09889ba5bafe753508ab7e03e9b581c37
kevinfischer <fischer.kevin.a@gmail.com>
2023-05-25 18:08:58 -0400

    Bot v0

--- bot.js
+++ bot.js
@@ -1,7 +1,29 @@
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
-
 const { Soul, Blueprints } = require('socialagi');
+
 const samantha = new Soul(Blueprints.SAMANTHA);
+const page = new Soul(Blueprints.PAGE);
+const reggie = new Soul(Blueprints.REGGIE);
+const dustin = new Soul(Blueprints.DUSTIN);
+
+const channelToSoul = {
+  '1111126482912284723': {
+    soul: dustin,
+    profileImg: 'https://cdn.discordapp.com/attachments/1103405119766282491/1111125819847352330/dusty.png'
+  },
+  '1111126681994932244': {
+    soul: samantha,
+    profileImg: 'https://cdn.discordapp.com/attachments/1103405119766282491/1111129745271296030/Screen_Shot_2023-05-24_at_11.12.22_PM.png'
+  },
+  '1111126931916718090': {
+    soul: page,
+    profileImg: 'https://cdn.discordapp.com/attachments/1103405119766282491/1111127124091342868/page.png'
+  },
+  '1111127239577309266': {
+    soul: reggie,
+    profileImg: 'https://cdn.discordapp.com/attachments/1103405119766282491/1111126842125066250/reggie.png'
+  },
+};
 
 const client = new Client({
   intents: [
@@ -10,8 +32,6 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
-const channelId = '1111105938603114566';
-
 
 const profiles = new Map();
 
@@ -72,22 +92,26 @@ client.on('interactionCreate', async (interaction) => {
 client.on('messageCreate', async message => {
   if (message.author.bot) return;
 
-  console.log('got message', message.content);
-  if (message.channel.id === channelId) {
-    samantha.tell(`${message.author.username} says ${message.content}`);
+  console.log('got message', message.content, message.type);
+  if (Object.keys(channelToSoul).includes(message.channel.id) && message.type === 0) {
+    channelToSoul[message.channel.id].soul.tell(`${message.author.username} says ${message.content}`);
   }
 });
 
-samantha.on('says', message => {
-  console.warn('SEND MESSAGE', message);
-  const exampleEmbed = new EmbedBuilder()
-    .setColor(0x0099FF)
-    .setAuthor({ name: 'Samantha', iconURL: 'https://i2-prod.dailystar.co.uk/incoming/article24246568.ece/ALTERNATES/s1200c/1_JS236355871.jpg' })
-    .setThumbnail('https://i2-prod.dailystar.co.uk/incoming/article24246568.ece/ALTERNATES/s1200c/1_JS236355871.jpg')
-    .setDescription(message);
+for (const channelId of Object.keys(channelToSoul)) {
+  const soul = channelToSoul[channelId].soul;
+  const profileImg = channelToSoul[channelId].profileImg;
+  soul.on('says', message => {
+    console.warn('SEND MESSAGE for', soul.blueprint.name, message);
+    const exampleEmbed = new EmbedBuilder()
+      .setColor(0x0099FF)
+      .setAuthor({ name: soul.blueprint.name, iconURL:profileImg })
+      .setThumbnail(profileImg)
+      .setDescription(message);
 
-  const channel = client.channels.cache.get(channelId);
-  channel.send({ embeds: [exampleEmbed] });
-});
+    const channel = client.channels.cache.get(channelId);
+    channel.send({ embeds: [exampleEmbed] });
+  });
+}
 
 client.login(process.env.DISCORD_TOKEN);
